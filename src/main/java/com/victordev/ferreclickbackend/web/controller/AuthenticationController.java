c3cdd02a3cb2f033e5a073424e1298c1ccba865d
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-08 07:33:12 -0500

    feat: add register, login and logout authentication controller

---
+++ src/main/java/com/victordev/ferreclickbackend/web/controller/AuthenticationController.java
@@ -0,0 +1,51 @@
+package com.victordev.ferreclickbackend.web.controller;
+
+import com.victordev.ferreclickbackend.dto.api.RegistrationBody;
+import com.victordev.ferreclickbackend.dto.security.LoginRequest;
+import com.victordev.ferreclickbackend.dto.security.LoginResponse;
+import com.victordev.ferreclickbackend.exception.UserAlreadyExistsException;
+import com.victordev.ferreclickbackend.service.IUserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PreAuthorize("permitAll()")
+    @PostMapping("/register")
+    public ResponseEntity registerUser(@RequestBody @Valid RegistrationBody registrationBody) {
+        try{
+            userService.registerUser(registrationBody);
+            return ResponseEntity.ok().build();
+        }catch (UserAlreadyExistsException ex){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+    @PreAuthorize("permitAll()")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody @Valid LoginRequest loginRequest) {
+        try {
+            return ResponseEntity.ok(userService.loginUser(loginRequest));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @PreAuthorize("permitAll()")
+    @PostMapping("/logout")
+    public void logout() throws Exception{
+        userService.logout();
+    }
+}
