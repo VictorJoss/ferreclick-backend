3ebdeb0097b33917e4ec8291207dbd7af250500b
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-08 04:49:43 -0500

    refactor: add username attribute and UserDetails implementation

--- src/main/java/com/victordev/ferreclickbackend/persistence/entity/User.java
+++ src/main/java/com/victordev/ferreclickbackend/persistence/entity/User.java
@@ -3,19 +3,72 @@ package com.victordev.ferreclickbackend.persistence.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String username;
     private String email;
     private String password;
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+    private boolean accountExpired;
+    private boolean accountLocked;
+    private boolean credentialsExpired;
+    private boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == null){
+            return null;
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(this.role.getAuthority()));
+
+        if(this.role.getPermissions() == null){
+            return authorities;
+        }
+
+        role.getPermissions().stream()
+                .forEach(each -> {
+                    String permissionName = each.getPermission().getName();
+                    authorities.add(new SimpleGrantedAuthority(permissionName));
+                });
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !accountExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !credentialsExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
