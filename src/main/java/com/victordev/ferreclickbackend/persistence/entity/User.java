9bcc21313ccf83300d09b96857db38531dd4e325
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-07 22:33:00 -0500

    feat: add user entity

---
+++ src/main/java/com/victordev/ferreclickbackend/persistence/entity/User.java
@@ -0,0 +1,21 @@
+package com.victordev.ferreclickbackend.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
