6ea352f2b145619744a9c9e8d16ac2111651d7c9
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-08 07:31:02 -0500

    feat: add user and role repository

---
+++ src/main/java/com/victordev/ferreclickbackend/persistence/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.victordev.ferreclickbackend.persistence.repository;
+
+import com.victordev.ferreclickbackend.persistence.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailIgnoreCase(String email);
+    Optional<User> findByUsernameIgnoreCase(String username);
+}
