2f8f1a21af6f0bd239db5e8873186a4791bf624c
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-09 22:36:46 -0500

    feat: update and add authorization to product creation and getting endpoints

--- src/main/java/com/victordev/ferreclickbackend/web/controller/ProductController.java
+++ src/main/java/com/victordev/ferreclickbackend/web/controller/ProductController.java
@@ -1,31 +1,47 @@
 package com.victordev.ferreclickbackend.web.controller;
 
+import com.victordev.ferreclickbackend.dto.api.ProductBody;
+import com.victordev.ferreclickbackend.dto.api.ProductResponse;
 import com.victordev.ferreclickbackend.persistence.entity.Product;
 import com.victordev.ferreclickbackend.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
     private IProductService productService;
 
-    @GetMapping("/all")
-    public ResponseEntity<List<Product>> getAllProducts() {
-        return ResponseEntity.ok(productService.getAllProducts());
+    @PreAuthorize("permitAll()")
+    @GetMapping
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
     }
 
-    @GetMapping("/{productId}")
-    public ResponseEntity<Product> getProductById(@PathVariable Long productId) {
-        return ResponseEntity.ok(productService.getProductById(productId));
+    @PreAuthorize("permitAll()")
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PreAuthorize("hasAuthority('product:create')")
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductBody productBody) {
+        Product product = new Product();
+        product.setName(productBody.getName());
+        product.setDescription(productBody.getDescription());
+        product.setImage(productBody.getImage());
+        product.setPrice(productBody.getPrice());
+
+        ProductResponse savedProduct = productService.createProduct(product, productBody.getCategoryIds());
+        return ResponseEntity.ok(savedProduct);
     }
 }
