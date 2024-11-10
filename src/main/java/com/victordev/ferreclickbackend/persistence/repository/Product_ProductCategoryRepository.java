ca015d2a8d540a5712e45161c29bde69005b5474
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-09 22:15:20 -0500

    feat: add product-productCategory repository

---
+++ src/main/java/com/victordev/ferreclickbackend/persistence/repository/Product_ProductCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.victordev.ferreclickbackend.persistence.repository;
+
+import com.victordev.ferreclickbackend.persistence.entity.Product_ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Product_ProductCategoryRepository extends JpaRepository<Product_ProductCategory, Long> {
+}
