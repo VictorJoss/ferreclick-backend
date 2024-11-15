377ea300dd2126f80a56b551f8f02f2631ae73d5
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-15 17:05:48 -0500

    feat: add query to find products by category ID

--- src/main/java/com/victordev/ferreclickbackend/persistence/repository/Product_ProductCategoryRepository.java
+++ src/main/java/com/victordev/ferreclickbackend/persistence/repository/Product_ProductCategoryRepository.java
@@ -1,7 +1,15 @@
 package com.victordev.ferreclickbackend.persistence.repository;
 
+import com.victordev.ferreclickbackend.persistence.entity.Product;
 import com.victordev.ferreclickbackend.persistence.entity.Product_ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface Product_ProductCategoryRepository extends JpaRepository<Product_ProductCategory, Long> {
+
+    @Query("SELECT p.product FROM Product_ProductCategory p WHERE p.category.id = :categoryId")
+    List<Product> findProductsByCategoryId(@Param("categoryId") Long categoryId);
 }
