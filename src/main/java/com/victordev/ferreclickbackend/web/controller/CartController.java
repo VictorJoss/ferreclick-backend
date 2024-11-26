1b8b17e0d52c20ada6ecf818cc15d6e28b789129
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-26 18:52:01 -0500

    feat: add cart controller

---
+++ src/main/java/com/victordev/ferreclickbackend/web/controller/CartController.java
@@ -0,0 +1,37 @@
+package com.victordev.ferreclickbackend.web.controller;
+
+import com.victordev.ferreclickbackend.dto.api.AddToCartRequest;
+import com.victordev.ferreclickbackend.dto.api.AddedToCartResponse;
+import com.victordev.ferreclickbackend.dto.api.CartResponse;
+import com.victordev.ferreclickbackend.service.ICartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+
+    @Autowired
+    private ICartService cartService;
+
+    @PreAuthorize("permitAll()")
+    @PostMapping("/add-product")
+    public ResponseEntity<AddedToCartResponse> addProductToCart(@RequestBody AddToCartRequest addToCartRequest){
+        return ResponseEntity.ok(cartService.addProductToCart(addToCartRequest));
+    }
+
+    @PreAuthorize("permitAll()")
+    @GetMapping("/{userId}")
+    public ResponseEntity<CartResponse> getCartByUserId(@PathVariable Long userId){
+        return ResponseEntity.ok(cartService.getCartByUserId(userId));
+    }
+
+    @PreAuthorize("permitAll()")
+    @DeleteMapping("/remove-product/{userId}/{productId}")
+    public ResponseEntity<Void> removeProductFromCart(@PathVariable Long userId, @PathVariable Long productId) {
+        cartService.removeProductFromCart(userId, productId);
+        return ResponseEntity.noContent().build();
+    }
+}
