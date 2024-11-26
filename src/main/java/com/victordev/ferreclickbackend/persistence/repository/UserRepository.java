1dc3498c71fd249516c1223071f3d99c8e969f59
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-26 18:56:32 -0500

    feat: add optional method for find ser by id

--- src/main/java/com/victordev/ferreclickbackend/persistence/repository/UserRepository.java
+++ src/main/java/com/victordev/ferreclickbackend/persistence/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailIgnoreCase(String email);
     Optional<User> findByUsernameIgnoreCase(String username);
+    Optional<User> findById(Long id);
 }
