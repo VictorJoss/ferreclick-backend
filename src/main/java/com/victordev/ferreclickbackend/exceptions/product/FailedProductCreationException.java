d9af62f6dab3aa029be400f4db98b3bfc4c023cc
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2025-02-13 23:41:00 -0500

    rename productCreationException

--- src/main/java/com/victordev/ferreclickbackend/exceptions/product/FailedProductCreationException.java
+++ src/main/java/com/victordev/ferreclickbackend/exceptions/product/FailedProductCreationException.java
@@ -3,13 +3,13 @@ package com.victordev.ferreclickbackend.exceptions.product;
 /**
  * Excepción lanzada cuando ocurre un error al crear un producto.
  */
-public class ProductCreationException extends RuntimeException{
+public class FailedProductCreationException extends RuntimeException{
 
     /**
      * Constructor de la excepción.
      * @param message Mensaje de error.
      */
-    public ProductCreationException(String message, Throwable cause) {
+    public FailedProductCreationException(String message, Throwable cause) {
         super(message, cause);
     }
 }
