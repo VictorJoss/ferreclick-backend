8f814d0d2cdcf8aa1aa9205ee3abeb10ac5f33b1
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2025-02-14 23:11:51 -0500

    refactor: add validating annotations

--- src/main/java/com/victordev/ferreclickbackend/dto/api/ProductBody.java
+++ src/main/java/com/victordev/ferreclickbackend/dto/api/ProductBody.java
@@ -1,5 +1,6 @@
 package com.victordev.ferreclickbackend.dto.api;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,21 +17,33 @@ public class ProductBody {
     /**
      * Nombre del producto.
      */
+    @NotNull
+    @NotBlank
     private String name;
     /**
      * Descripción del producto.
      */
+    @NotNull
+    @NotBlank
+    @Size(max = 255)
     private String description;
     /**
      * Imagen del producto.
      */
+    @NotNull
+    @NotBlank
     private MultipartFile image;
     /**
      * Precio del producto.
      */
+    @NotNull
+    @NotBlank
+    @Positive
     private Double price;
     /**
      * Lista de identificadores de las categorías a las que pertenece el producto.
      */
+    @NotNull
+    @NotEmpty
     private List<Long> categoryIds;
 }
--- src/main/java/com/victordev/ferreclickbackend/dto/api/UpdateProductBody.java
+++ src/main/java/com/victordev/ferreclickbackend/dto/api/UpdateProductBody.java
@@ -1,5 +1,9 @@
 package com.victordev.ferreclickbackend.dto.api;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,22 +20,34 @@ public class UpdateProductBody {
     /**
      * Identificador del producto.
      */
+    @NotNull
+    @NotBlank
     private Long id;
     /**
      * Nombre del producto.
      */
+    @NotNull
+    @NotBlank
     private String name;
     /**
      * Descripción del producto.
      */
+    @NotNull
+    @NotBlank
+    @Size(max = 255)
     private String description;
     /**
      * Imagen del producto.
      */
+    @NotNull
+    @NotBlank
     private MultipartFile image;
     /**
      * Precio del producto.
      */
+    @NotNull
+    @NotBlank
+    @Positive
     private Double price;
     /**
      * Lista de identificadores de las categorías a las que pertenece el producto.
